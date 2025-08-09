bc91cdbd60dba41481205f36f76ae87775750ed9
Vladimir Sukhov <vdsukhov@ya.ru>
2025-08-09 13:29:14 -0500

    Github actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Home from './pages/Home.jsx';
 import Projects from './pages/Projects.jsx';
